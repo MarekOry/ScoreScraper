6a47ac4cf7b79b10c121b864b2f7ff7ca9bc08a7
marek <marek@noreply.com>
2024-08-30 12:16:55 +0200

    Initial commit

---
+++ src/main/java/pl/marek/scorescraper/ScoreScraperApplication.java
@@ -0,0 +1,12 @@
+package pl.marek.scorescraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoreScraperApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/marek/scorescraper/ScoreScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marek.scorescraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
